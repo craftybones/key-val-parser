3697dfad5f4fbaa662f8a8f07da17dfae099cd2c
Srijayanth Sridhar <srijayanth@gmail.com>
2017-07-25 20:17:16 +0530

    Added strictParseInfo as a module.export.

--- src/strictParseInfo.js
+++ src/strictParseInfo.js
@@ -15,9 +15,10 @@ var StrictParseInfo=function(initialParsingFunction,validKeys) {
 StrictParseInfo.prototype=Object.create(ParseInfo.prototype);
 
 StrictParseInfo.prototype.pushKeyValuePair=function() {
-  console.log("I'm here");
   if(!contains(this.validKeys,this.currentKey))
     throw new Error("invalid key");
   this.parsedKeys[this.currentKey]=this.currentValue;
   this.resetKeysAndValues();
 }
+
+module.exports=StrictParseInfo;
