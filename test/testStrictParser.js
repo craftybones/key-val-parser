afbb532f5ac88d1e6f60e573c185ae6c8d209f5c
Srijayanth Sridhar <srijayanth@gmail.com>
2017-07-26 12:31:53 +0530

    One more test with multiple keys

--- test/testStrictParser.js
+++ test/testStrictParser.js
@@ -2,11 +2,22 @@ const assert=require('assert');
 const StrictParser=require('../src/index.js').StrictParser;
 
 describe("strict parser",function(){
-  it("should only parse keys that are specified",function(){
+  it("should only parse keys that are specified for a single key",function(){
     let kvParser=new StrictParser(["name"]);
     assert.throws(
       () => {
         var p=kvParser.parse("age=23");
+      },Error)
+  });
+
+  it("should only parse keys that are specified for multiple keys",function(){
+    let kvParser=new StrictParser(["name","age"]);
+    let actual=kvParser.parse("name=john age=23");
+    let expected={name:"john",age:"23"};
+    assert.deepEqual(expected,actual);
+    assert.throws(
+      () => {
+        var p=kvParser.parse("color=blue");
       },Error)
   });
 });
